93f99c5a49f4fdd381a2ed02cb0ce668b73d093d
DoubleX <DoubleX@localhost.localdomain>
2020-06-10 14:30:04 -0400

    First Commit

---
+++ median_filter.cu
@@ -0,0 +1,148 @@
+#include <stdio.h>
+
+// Macro for checking errors in CUDA API calls
+#define cudaErrorCheck(call)                                                              \
+do{                                                                                       \
+    cudaError_t cuErr = call;                                                             \
+    if(cudaSuccess != cuErr){                                                             \
+      printf("CUDA Error - %s:%d: '%s'\n", __FILE__, __LINE__, cudaGetErrorString(cuErr));\
+      exit(0);                                                                            \
+    }                                                                                     \
+}while(0)
+
+// Values for MxN matrix
+#define M 10
+#define N 10
+
+// Kernel
+__global__ void add_matrices(int *a, int *b, int *c)
+{
+	int column = blockDim.x * blockIdx.x + threadIdx.x;
+	int row    = blockDim.y * blockIdx.y + threadIdx.y;
+	int sum = 0;
+	if (row < M && column < N)
+	{
+		int thread_id = row * N + column;
+		//c[thread_id] = a[thread_id] + b[thread_id];
+		//c[thread_id] = a[thread_id] + 1;
+		for (int i = 0; i < 25; i++)
+		{
+			//if (thread_id == 1)
+				//printf("%d %d, ", a[thread_id], b[i]);
+			//if (thread_id == 2)
+				//printf("%d %d, ", a[thread_id], b[i]);
+			sum = sum + (a[thread_id] * b[i]);
+		}
+		c[thread_id] = sum;
+	}
+}
+
+// Main program
+int main()
+{
+	// Number of bytes to allocate for MxN matrix
+	size_t bytes = M*N*sizeof(int);
+
+	// Allocate memory for arrays A, B, and C on host
+	int A[M][N];
+	int C[M][N];
+
+	
+
+	int B[5][5] =
+    	{
+		{0,1,1,1,0},
+		{1,2,2,2,1},
+		{1,2,4,2,1},
+		{1,2,2,2,1},
+		{0,1,1,1,0}
+    	};
+
+	for(int i=0; i<5; i++)
+	{
+		for(int j=0; j<5; j++)
+		{
+			printf("%d", B[i][j]);
+		}
+		printf("\n");
+	}
+
+	
+
+	// Allocate memory for arrays d_A, d_B, and d_C on device
+	int *d_A, *d_B, *d_C;
+	cudaErrorCheck( cudaMalloc(&d_A, bytes) );
+	cudaErrorCheck( cudaMalloc(&d_B, bytes) );
+	cudaErrorCheck( cudaMalloc(&d_C, bytes) );
+
+	// Initialize host arrays A and B
+	for(int i=0; i<M; i++)
+	{
+		for(int j=0; j<N; j++)
+		{
+			A[i][j] = 1;
+		}
+	}
+
+	
+
+	// Copy data from host arrays A and B to device arrays d_A and d_B
+	cudaErrorCheck( cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice) );
+	cudaErrorCheck( cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice) );
+
+	// Set execution configuration parameters
+	// 		threads_per_block: number of CUDA threads per grid block
+	//		blocks_in_grid   : number of blocks in grid
+	//		(These are c structs with 3 member variables x, y, x)
+	dim3 threads_per_block( 16, 16, 1 );
+	dim3 blocks_in_grid( ceil( float(N) / threads_per_block.x ), ceil( float(M) / threads_per_block.y ), 1 );
+
+	// Launch kernel
+	add_matrices<<< blocks_in_grid, threads_per_block >>>(d_A, d_B, d_C);
+
+	// Check for errors in kernel launch (e.g. invalid execution configuration paramters)
+  cudaError_t cuErrSync  = cudaGetLastError();
+
+	// Check for errors on the GPU after control is returned to CPU
+  cudaError_t cuErrAsync = cudaDeviceSynchronize();
+
+  if (cuErrSync != cudaSuccess) 
+	{ printf("CUDA Error - %s:%d: '%s'\n", __FILE__, __LINE__, cudaGetErrorString(cuErrSync)); exit(0); }
+
+  if (cuErrAsync != cudaSuccess) 
+	{ printf("CUDA Error - %s:%d: '%s'\n", __FILE__, __LINE__, cudaGetErrorString(cuErrAsync)); exit(0); }
+
+	// Copy data from device array d_C to host array C
+	cudaErrorCheck( cudaMemcpy(C, d_C, bytes, cudaMemcpyDeviceToHost) );
+
+	// Verify results
+	printf("Displaying data\n\n");
+	
+
+	for(int i=0; i<M; i++)
+	{
+		for(int j=0; j<N; j++)
+		{
+			printf("%d ", C[i][j]);
+		}
+		printf("\n");
+	}
+
+	// Free GPU memory
+	cudaErrorCheck( cudaFree(d_A) );
+	cudaErrorCheck( cudaFree(d_B) );
+	cudaErrorCheck( cudaFree(d_C) );
+
+  printf("\n--------------------------------\n");
+  printf("__SUCCESS__\n");
+  printf("--------------------------------\n");
+  printf("M                         = %d\n", M);
+	printf("N                         = %d\n", N);
+  printf("Threads Per Block (x-dim) = %d\n", threads_per_block.x);
+  printf("Threads Per Block (y-dim) = %d\n", threads_per_block.y);
+  printf("Blocks In Grid (x-dim)    = %d\n", blocks_in_grid.x);
+	printf("Blocks In Grid (y-dim)    = %d\n", blocks_in_grid.y);
+  printf("--------------------------------\n\n");
+
+	return 0;
+}
