d8e223b8ed938d2e207cfe93a939caf960ffb92e
DoubleX <DoubleX@localhost.localdomain>
2020-06-10 17:01:33 -0400

    Second Commit

--- median_filter.cu
+++ median_filter.cu
@@ -19,21 +19,66 @@ __global__ void add_matrices(int *a, int *b, int *c)
 {
 	int column = blockDim.x * blockIdx.x + threadIdx.x;
 	int row    = blockDim.y * blockIdx.y + threadIdx.y;
-	int sum = 0;
+
+	int array[32];
+	int count = 0;
+
 	if (row < M && column < N)
 	{
 		int thread_id = row * N + column;
 		//c[thread_id] = a[thread_id] + b[thread_id];
 		//c[thread_id] = a[thread_id] + 1;
-		for (int i = 0; i < 25; i++)
+
+		if(row > 1 && column > 1 && row < M - 2 && column < N - 2)
 		{
-			//if (thread_id == 1)
-				//printf("%d %d, ", a[thread_id], b[i]);
-			//if (thread_id == 2)
-				//printf("%d %d, ", a[thread_id], b[i]);
-			sum = sum + (a[thread_id] * b[i]);
+			for(int i = 0 ; i < 25; i++)
+			{
+				for (int j = 0 ; j < b[i]; j++)
+				{
+					if (i < 5)
+						array[count] = a[thread_id - (2*N) - 2 + i];
+
+					else if (i < 10)
+						array[count] = a[thread_id - N - 2 + i - 5];
+
+					else if (i < 15)
+						array[count] = a[thread_id - 2 + i - 10];
+
+					else if (i < 20)
+						array[count] = a[thread_id + N - 2 + i - 15];
+
+					else if (i < 25)
+						array[count] = a[thread_id + (2*N) - 2 + i - 20];
+						
+					count++;
+				}
+			}
+
+			printf("displaying the array \n");
+			
+			for (int i = 0 ; i < 32; i++)
+			{printf("%d ", array[i]);}
+
+
+			for (int i = 0 ; i < 32; i++)
+			{
+				for (int j = 0 ; j < 32; j++)
+				{
+					if (array[j] > array [j + 1])
+					{
+						int temp = array[j];
+						array[j] = array[j+1];
+						array[j+1] = temp;
+					}
+				}
+			}			
+
+		c[thread_id] = array[32/2];
 		}
-		c[thread_id] = sum;
+		//int num = a[thread_id];
+		
+		
+
 	}
 }
 
@@ -44,7 +89,22 @@ int main()
 	size_t bytes = M*N*sizeof(int);
 
 	// Allocate memory for arrays A, B, and C on host
-	int A[M][N];
+	int A[M][N]=
+    	{
+		{249,255,252,255,235,0,255,255,255,243},
+		{244,255,241,255,255,253,252,0,255,233},
+		{255,255,242,248,249,239,248,3,237,255},
+		{245,244,254,255,255,255,250,255,255,238},
+		{255,241,255,242,255,236,255,0,254,251},
+		{253,255,244,255,250,255,245,251,252,255},
+		{233,255,248,239,255,243,255,251,4,0},
+		{255,240,252,252,255,252,238,255,252,255},
+		{255,248,253,247,255,252,255,247,253,255},
+		{250,0,251,255,246,247,240,255,246,244}
+    	};
+
+
+
 	int C[M][N];
 
 	
@@ -80,7 +140,7 @@ int main()
 	{
 		for(int j=0; j<N; j++)
 		{
-			A[i][j] = 1;
+			C[i][j] = 1;
 		}
 	}
 
@@ -123,7 +183,7 @@ int main()
 	{
 		for(int j=0; j<N; j++)
 		{
-			printf("%d ", C[i][j]);
+			printf("%d\t", C[i][j]);
 		}
 		printf("\n");
 	}
